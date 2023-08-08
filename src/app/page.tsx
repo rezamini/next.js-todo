149c6a1573cc6efd35fac6d02d494d29bab4874d
Reza <reza.amini.b@gmail.com>
2023-08-08 11:38:26 +0100

    - added prisma query to get the list of ... todos and displaying them in the main page. This utlises the server rendering and database query in the server instead of the client(which react does) but written in a component. Only raw data and results are sent to client and server information are also secured in the server.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home(){
+export default async function Home(){
+  const todos = await getTodos();
+
+  // await prisma.todo.create({data : {title: "test", complete: true}})
+
   return <>
     <header className="flex justify-between items-center">
       <h1 className="text-2xl">Todos</h1>
       <Link href="/new" className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
     </header>
-    <ul className="pl-4"></ul>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <li key={todo.id}> {todo.title} </li>
+      ))}
+      
+    </ul>
   </>
+}
+
+function getTodos(){
+  return prisma.todo.findMany();
 }
