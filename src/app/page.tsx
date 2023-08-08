c3b4a837e2dee9defa0d3ea6382811d654a86562
Reza <reza.amini.b@gmail.com>
2023-08-08 11:20:37 +0100

    - added main page elements and required tailwind.. classnames and designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home(){
-  return <h1>Test</h1>
+  return <>
+    <header className="flex justify-between items-center">
+      <h1 className="text-2xl">Todos</h1>
+      <Link href="/new" className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
+    </header>
+    <ul className="pl-4"></ul>
+  </>
 }
