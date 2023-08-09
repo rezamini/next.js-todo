f23cf6a48941b2400df13558306f0e90bb928076
Reza <reza.amini.b@gmail.com>
2023-08-09 12:23:10 +0100

    - added new tailwind classnames for todo item... labels

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,7 +8,7 @@ export function TodoItem({ id, title, complete }: TodoItemProps) {
   return (
     <li className="flex gap-1 items-center">
       <input id={id} type="checkbox" className="cursor-pointer peer" />
-      <label htmlFor={id} className="peer-checked:line-through">
+      <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-zinc-400">
         {title}
       </label>
     </li>
