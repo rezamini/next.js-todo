d50ba14c782aa7d12da0cd656e646f4af829e9b4
Reza <reza.amini.b@gmail.com>
2023-08-09 12:08:55 +0100

    - added input and label contents for todo item ... component with their classnames and design

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -6,6 +6,11 @@ type TodoItemProps = {
 
 export function TodoItem({ id, title, complete }: TodoItemProps) {
   return (
-    <></>
+    <li className="flex gap-1 items-center">
+      <input id={id} type="checkbox" className="cursor-pointer peer" />
+      <label htmlFor={id} className="peer-checked:line-through">
+        {title}
+      </label>
+    </li>
   );
 }
