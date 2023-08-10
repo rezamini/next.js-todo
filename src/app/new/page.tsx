e85a5a4f6bbd5990f2d56477a96c1a6c810fe281
Reza <reza.amini.b@gmail.com>
2023-08-10 10:21:02 +0100

    - added new todo item page elements and respective designs

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
 export default function New() {
-  return <h1>New Page</h1>
+  return (
+    <>
+      <header>
+        <h1 className="text-2xl">New Todo Item</h1>
+      </header>
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 focus-within:border-zinc-700 bg-transparent outline-none"
+        ></input>
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 outline-none"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
