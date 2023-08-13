a727dde3b25008ad7c33246ceca5ac8bcaedea7d
Reza <reza.amini.b@gmail.com>
2023-08-13 20:06:55 +0100

    - updated the todo item props types with... a new method type

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,29 @@
+"use client"
+
 type TodoItemProps = {
   id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
 };
 
 export function TodoItem({ id, title, complete }: TodoItemProps) {
   return (
     <li className="flex gap-1 items-center">
-      <input id={id} type="checkbox" className="cursor-pointer peer" />
-      <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-zinc-400">
+      <input
+        id={id}
+        type="checkbox"
+        defaultChecked={complete}
+        className="cursor-pointer peer"
+      />
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-zinc-400"
+      >
         {title}
       </label>
     </li>
   );
 }
+
+
