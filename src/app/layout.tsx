a761929a8e3de775c733bd44587b4bd51fab56bd
Reza <reza.amini.b@gmail.com>
2023-08-15 12:13:19 +0100

    - added main layout max width and padding... in order to center the layout and elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Todo Next.js App',
-}
+  title: "Todo Next.js App",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-800 text-slate-100 container mx-auto p-4 `}>{children}</body>
+      <body
+        className={`${inter.className} bg-zinc-800 text-slate-100 container mx-auto p-10 max-w-3xl`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
