6092214839cdc50511d733b174862a801d0216d9
Reza <reza.amini.b@gmail.com>
2023-08-17 11:13:38 +0100

    - added placeholder for the todo item input...

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -12,6 +12,7 @@ export default function New() {
         <input
           type="text"
           name="title"
+          placeholder="Create a new todo item"
           className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 focus-within:border-zinc-700 bg-transparent outline-none"
         ></input>
         <div className="flex gap-1 justify-end">
