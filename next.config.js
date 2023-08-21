e1c451465ab621e8272fd0583eb611c46a33c413
Reza <reza.amini.b@gmail.com>
2023-08-21 11:07:02 +0100

    - added and enabled static export config ... for the app router

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     experimental: {
         serverActions: true,
-    }
+    },
 }
 
 module.exports = nextConfig
