bf8d572e88fc59afb0f8c1afe46c921c418630f4
Reza <reza.amini.b@gmail.com>
2023-08-22 12:34:17 +0100

    -changed the db from sqlite to supabase postgresql

---
+++ prisma/migrations/20230822112803_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "complete" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
