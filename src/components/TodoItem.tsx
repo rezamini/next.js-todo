0d76880309eff8843b14596e0db92830a0a2f0ea
Reza <reza.amini.b@gmail.com>
2023-09-04 13:01:47 +0100

    - updated and utilised useState hook for the... TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,6 @@
-"use client"
+"use client";
+
+import { useState } from "react";
 
 type TodoItemProps = {
   id: string;
@@ -7,24 +9,35 @@ type TodoItemProps = {
   toggleTodo: (id: string, complete: boolean) => void;
 };
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+// export function TodoItem({ id, title, complete, toggleTodo, deleteTodo }: TodoItemProps) {
+export function TodoItem( props : TodoItemProps) {
+  // const [data, setData] = useState<TodoItemProps>({ id, title, complete, toggleTodo, deleteTodo });
+  const [data, setData] = useState<TodoItemProps>(props);
+
+  async function performToggleTodo(id: string, complete: boolean){
+    data.toggleTodo(id, complete);
+    setData(data => ({...data, complete: complete}));
+    // setData({...data});
+
+  }
+
   return (
     <li className="flex gap-1 items-center">
       <input
-        id={id}
+        id={data.id}
         type="checkbox"
-        defaultChecked={complete}
-        onChange={e => toggleTodo(id, e.target.checked)}
+        defaultChecked={data.complete}
+        // onChange={(e) => toggleTodo(id, e.target.checked)}
+        onChange={(e) => performToggleTodo(data.id, e.target.checked)}
         className="cursor-pointer peer accent-zinc-400"
       />
       <label
-        htmlFor={id}
+        htmlFor={data.id}
         className="cursor-pointer peer-checked:line-through peer-checked:text-zinc-400"
       >
-        {title}
+        {data.title}
       </label>
+      
     </li>
   );
 }
-
-
